cd04a323e340770cb27be8c49e7194869a40f6d1
Sheraff <fpellet@ensc.fr>
2024-02-16 00:54:00 +0100

    add algolia logo

--- components/AlgoliaSearch.tsx
+++ components/AlgoliaSearch.tsx
@@ -2,10 +2,11 @@
 import algoliasearch from 'algoliasearch/lite'
 import type { SharedProps } from 'fumadocs-ui/components/dialog/search'
 import SearchDialog from 'fumadocs-ui/components/dialog/search-algolia'
+import Image from "next/image"
 
 const client = algoliasearch(process.env.NEXT_PUBLIC_ALGOLIA_APP_ID!, process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY!)
 const index = client.initIndex("document")
 
 export function AlgoliaSearch(props: SharedProps) {
-	return <SearchDialog index={index} {...props} />
+	return <SearchDialog index={index} {...props} footer={<Image src="/algolia.svg" alt="Algolia logo" width={80} height={20} style={{ marginLeft: "auto" }} />} />
 }
