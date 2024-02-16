e41b878fb18307ccf1b4e7c2a15298fe8632524a
Sheraff <fpellet@ensc.fr>
2024-02-16 10:08:37 +0100

    add icon, favicon and page meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { pageTree } from './source'
 import { DocsLayout } from 'fumadocs-ui/layout'
 import { AlgoliaSearch } from "@/components/AlgoliaSearch"
 import { StaticContextProvider } from "@/components/StaticContext"
+import type { Metadata } from 'next/types'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -17,14 +18,21 @@ const staticParams = {
   algoliaSearchApiKey: process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY!
 }
 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | 🌳 Root Docs',
+    default: '🌳 Root Docs',
+  },
+  description: 'The offline-first fullstack PWA template.',
+}
+
 export default function Layout({ children }: { children: ReactNode }) {
-  console.log(staticParams)
   return (
     <html lang="en" className={inter.className}>
       <body>
         <StaticContextProvider value={staticParams}>
           <RootProvider search={{ SearchDialog: AlgoliaSearch }}>
-            <DocsLayout tree={pageTree} nav={{ title: 'My App', githubUrl: "https://github.com/Sheraff/root", enabled: true, }} >
+            <DocsLayout tree={pageTree} nav={{ title: '🌳 Root Docs', githubUrl: "https://github.com/Sheraff/root", enabled: true }} >
               {children}
             </DocsLayout>
           </RootProvider>
--- app/source.ts
+++ app/source.ts
@@ -1,9 +1,15 @@
 import { map } from '.map'
 import { createMDXSource } from 'fumadocs-mdx'
 import { loader } from 'fumadocs-core/source'
+import { create } from "components/Icon"
+import { icons } from 'lucide-react'
 
 export const { getPage, getPages, pageTree } = loader({
   baseUrl: '/',
   rootDir: 'docs',
   source: createMDXSource(map),
+  icon(icon) {
+    if (icon in icons)
+      return create({ icon: icons[icon as keyof typeof icons] })
+  },
 })
